691b16a9f98ef121d58a4624fccfdb5dc39abc75
Michael Hart <mhart@cloudflare.com>
2025-10-01 15:31:51 +1000

    Remove WelcomeToast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import { CartProvider } from "components/cart/cart-context";
 import { Navbar } from "components/layout/navbar";
-import { WelcomeToast } from "components/welcome-toast";
 import { GeistSans } from "geist/font/sans";
 import { getCart } from "lib/shopify";
 import { ReactNode } from "react";
-import { Toaster } from "sonner";
 import "./globals.css";
 import { baseUrl } from "lib/utils";
 
@@ -35,11 +33,7 @@ export default async function RootLayout({
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <CartProvider cartPromise={cart}>
           <Navbar />
-          <main>
-            {children}
-            <Toaster closeButton />
-            <WelcomeToast />
-          </main>
+          <main>{children}</main>
         </CartProvider>
       </body>
     </html>
